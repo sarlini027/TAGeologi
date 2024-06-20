6e5bdd0c447ff1337d5e129a9bb5e16a88cda27b
sarlini027 <sarlinilini8@gmail.com>
2024-06-20 21:33:29 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+// routes adalah controller mana yang akan merespon sebuah request, 
+// misal ketika mengakses /homeadmin maka controlle yang akan digunakan adalah Admincontroller dan fungsi index
+
+$routes->get('/', 'Home::index');
+$routes->get('/homeadmin', 'AdminController::index');
